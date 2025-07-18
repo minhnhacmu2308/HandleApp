c04d3878c9f53a02588e1e0cc1bb9054fc7a9f15
nguyenminhnha <nguyenminhnhacmu@gmail.com>
2025-07-18 16:18:52 +0700

    donme

---
+++ api/index.py
@@ -0,0 +1,4 @@
+from app import app
+
+# Export app cho Vercel
+handler = app 
--- app.py
+++ app.py
@@ -851,4 +851,4 @@ def skip_download():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8081) 
+    app.run(debug=True, host='0.0.0.0', port=8085) 
